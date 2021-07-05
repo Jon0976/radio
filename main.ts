853a89bf99ddc591b16615d2b2c69fb598171aba
Jon0976 <86691282+Jon0976@users.noreply.github.com>
2021-07-06 11:21:43 +1200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("y")
 })
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendString("n")
+})
+radio.setGroup(1)
+basic.showIcon(IconNames.Heart)
